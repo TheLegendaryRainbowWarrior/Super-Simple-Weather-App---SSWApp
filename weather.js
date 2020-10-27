c8e4685a9c9e0ed0e05fa36b03e4fbed5ab228f9
Print Monty <g.wedlich@me.com>
2020-10-27 21:41:47 +0100

    Typo fix.

--- weather.js
+++ weather.js
@@ -7,51 +7,49 @@ Author: Gregor Wedlich
 
 require('dotenv').config();
 
-const
-    yargs = require('yargs'),
+const yargs = require('yargs'),
     axios = require('axios');
 
 let arguments = yargs
     .option('c', {
-        'describe': 'Type your City e.g. -c amsterdam or with country code -c amsterdam,nl',
-        'alias': 'city',
-        'type': 'string',
-        'default': 'Amsterdam',
-        'demandOption': 'false'
+        describe: 'Type your City e.g. -c amsterdam or with country code -c amsterdam,nl',
+        alias: 'city',
+        type: 'string',
+        default: 'Amsterdam',
+        demandOption: 'false',
     })
     .option('u', {
-        'describe': 'For temperature in Fahrenheit use units=imperial, For temperature in Celsius use units=metric, For temperature in Kelvin use units=kelvin',
-        'alias': 'units',
-        'choices': ['imperial', 'metric', 'kelvin'],
-        'type': 'string',
-        'default': 'metric',
-        'demandOption': 'false'
+        describe: 'For temperature in Fahrenheit use units=imperial, For temperature in Celsius use units=metric, For temperature in Kelvin use units=kelvin',
+        alias: 'units',
+        choices: ['imperial', 'metric', 'kelvin'],
+        type: 'string',
+        default: 'metric',
+        demandOption: 'false',
     })
-    .help('h')
-    .argv;
-
+    .help('h').argv;
 
 const instance = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5/find',
-    timeout: 1000
+    timeout: 1000,
 });
 
-instance.get('/', {
+instance
+    .get('/', {
         params: {
             q: arguments.city,
             apiKey: process.env.API_KEY,
-            units: arguments.units
-        }
+            units: arguments.units,
+        },
     })
     .then(function (response) {
         response.data.list.forEach((data, index) => {
-            console.log((index + 1) + '. ' + 'Weather for: ' + data.name);
+            console.log(index + 1 + '. ' + 'Weather for: ' + data.name);
             console.log('   ' + 'Country: ' + data.sys.country);
             console.log('   ' + 'Temp: ' + data.main.temp);
             console.log('   ' + 'Temp minimal: ' + data.main.temp_min);
             console.log('   ' + 'Temp maximum: ' + data.main.temp_max);
             console.log('   ' + 'Temperature feels like: ' + data.main.feels_like);
-        })
+        });
     })
     .catch(function (error) {
         if (error.response) {
